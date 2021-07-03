c35b3eccb7cba3e7dfcd1dd3c888767ce3e525ee
SupravaKarmakar <suprava.k542@gmail.com>
2021-07-03 21:56:33 +0530

    Signup Alert and Remember me added

--- frontend/login.php
+++ frontend/login.php
@@ -1,6 +1,9 @@
 <?php
 include("connection.php");
 if(isset($_POST['submit'])){
+session_start();
+$_SESSION['rememail'] = $_REQUEST['email'];
+$_SESSION['rempass'] = $_REQUEST['password'];
 $qry = "SELECT * FROM `register_db` WHERE `email` = '".$_REQUEST['email']."'";
 if($result=mysqli_query($link, $qry))
 {
@@ -13,23 +16,40 @@ if($result=mysqli_query($link, $qry))
 			$rowc1 = mysqli_num_rows($result);
 			if($rowc1 == 0)
 			{   
-				echo "<script>alert('Incorrect password.');</script>";
+				$_SESSION['errorPass'] = "Incorrect Password!";
+				header('Location:sign.php');
                 
 			}
 			else
 			{
 				$row = mysqli_fetch_assoc($result);
-				session_start();
+				
 				$_SESSION['id'] = $row['id'];
 				$_SESSION['name'] = $row['uname'];
 				$_SESSION['email'] = $row['email'];
+				if(!empty($_REQUEST["remember"]))
+				{
+					setcookie("email",$_REQUEST['email'],time()+(10*365*24*60*60));
+					setcookie("password",$_REQUEST['password'],time()+(10*365*24*60*60));
+				}
+				else{
+					if(isset($_COOKIE["email"]))
+					{
+						setcookie("email","");
+					}
+					if(isset($_COOKIE["password"]))
+					{
+						setcookie("password","");
+					}
+				}
                 header('Location:userpanel.php');
 			}
 		}
 	}
 	else
 	{
-	    echo "<script>alert('Email id mismatched!!!');</script>";
+		$_SESSION['errorEmail'] = "Email Id Mismatched!";
+		header('Location:sign.php');
         
 	}
 }
