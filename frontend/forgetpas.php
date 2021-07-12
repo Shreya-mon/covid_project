3f909ade4e758266cf4fb56d2c49f7d0a6ce256f
Shreya-mon <monshreya123@gmail.com>
2021-07-12 20:29:02 +0200

    forget password changes

--- frontend/forgetpas.php
+++ frontend/forgetpas.php
@@ -50,8 +50,8 @@ if(isset($_REQUEST['submit']))
     $mail->isSMTP();                                            
     $mail->Host       = 'smtp.gmail.com;';                    
     $mail->SMTPAuth   = true;                             
-    $mail->Username   = 'monshreya123@gmail.com';                 
-    $mail->Password   = 'gokuspn69';
+    $mail->Username   = '';                 
+    $mail->Password   = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port       = 587;
   
